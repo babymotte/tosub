0fd83ce9321152782eb02fe907ddd7beeebf131e
Michael Bachmann <mbachmann@bbmsoft.net>
2026-02-06 16:46:47 +0100

    formatting

--- examples/panic_timeout.rs
+++ examples/panic_timeout.rs
@@ -27,7 +27,7 @@ async fn main() -> miette::Result<()> {
             fmt::Layer::new().with_writer(io::stderr).with_filter(
                 EnvFilter::builder()
                     .with_default_directive(LevelFilter::INFO.into())
-                                        .from_env_lossy(),
+                    .from_env_lossy(),
             ),
         )
         .init();
